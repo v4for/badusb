8e75aba3d1e8c745b832ecdd6f2198dfd18dcd93
Vafor <68476283+v4for@users.noreply.github.com>
2024-08-23 11:46:22 -0600

    Update and rename kencarson.bat to kencarson.ps1

---
+++ kencarson.ps1
@@ -0,0 +1,16 @@
+
+# volume.ps1
+Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+public class Volume {
+    [DllImport("user32.dll")]
+    public static extern int SendMessageW(IntPtr hWnd, int msg, int wParam, int lParam);
+    public const int WM_APPCOMMAND = 0x0319;
+    public const int APPCOMMAND_VOLUME_UP = 0x000a;
+    public static void VolumeUp() {
+        SendMessageW((IntPtr)0xffff, WM_APPCOMMAND, 0, APPCOMMAND_VOLUME_UP * 0x10000);
+    }
+}
+"@
+[Volume]::VolumeUp()
